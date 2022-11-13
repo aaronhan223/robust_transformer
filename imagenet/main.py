6a8a69189848d369573d1e9d6069084445d1947c
aaronhan223 <aaronhan223@gmail.com>
2022-11-12 21:52:03 -0600

    added spkde parameters

--- imagenet/main.py
+++ imagenet/main.py
@@ -34,6 +34,7 @@ def get_args_parser():
     parser.add_argument('--huber_a', default=0.2, type=float)
     parser.add_argument('--loss_type', default='huber', type=str)
     parser.add_argument('--num_iter_kirwls', default=1, type=int)
+    parser.add_argument('--outlier_fraction', default=0.05, type=float)
 
     # Model parameters
     parser.add_argument('--model', default='deit_base_patch16_224', type=str, metavar='MODEL',
